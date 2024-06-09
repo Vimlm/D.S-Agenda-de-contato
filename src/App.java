50fc19c63ab771683a075c1d0df77891603cc3c8
Victor Martins <VictorMLMartins@icloud.com>
2024-06-09 15:06:41 -0300

    feat: :sparkles: Implementação Menu
    Implementado lógicas de loop para funcionalidades dos menu's

--- src/App.java
+++ src/App.java
@@ -1,5 +1,77 @@
+import br.edu.up.view.Avisos;
+import br.edu.up.view.Menu;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+  public static void main(String[] args) throws Exception {
+    int opcao;
+
+    do {
+      opcao = Menu.principal();
+
+      switch (opcao) {
+        case 1:
+          int opcaoAdicionar;
+
+          do {
+            opcaoAdicionar = Menu.adicionarContato();
+
+            switch (opcaoAdicionar) {
+              case 1:
+                // Adicionar Contato Pessoal
+                break;
+
+              case 2:
+                // Adicionar Contato Comercial
+                break;
+
+              default:
+                Avisos.opcaoInvalida();
+                break;
+            }
+
+          } while (opcaoAdicionar != 0);
+          break;
+
+        case 2:
+          int opcaoExcluir;
+
+          do {
+            opcaoExcluir = Menu.excluirContato();
+
+            switch (opcaoExcluir) {
+              case 1:
+                //Excluir Contato Pessoal
+                break;
+
+              case 2:
+                //Excluir Contato Comercial
+                break;
+
+              default:
+                break;
+            }
+          } while (opcaoExcluir != 0);
+          break;
+
+        case 3:
+          // Mostrar Todos Os Contatos
+          break;
+
+        case 4:
+          String contato = Menu.mostrarContato();
+
+          //Pesquisar contato na base e mostrar o contato pesquisado.
+          break;
+
+        case 0:
+          Avisos.sair();
+          break;
+
+        default:
+          Avisos.opcaoInvalida();
+          break;
+      }
+
+    } while (opcao != 0);
+  }
 }
