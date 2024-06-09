526dd32cca3598c84b42f3e7c2f338487df7bf0b
Victor Martins <VictorMLMartins@icloud.com>
2024-06-09 15:06:15 -0300

    feat: :sparkles: View Avisos
    Realizado a criação da classe View de Avisos para avisos padrões no programa

---
+++ src/br/edu/up/view/Avisos.java
@@ -0,0 +1,11 @@
+package br.edu.up.view;
+
+public class Avisos {
+  public static void opcaoInvalida() {
+    System.out.println("\nOpção inválida, tente novamente.");
+  }
+
+  public static void sair() {
+    System.out.println("\nPrograma encerrado.");
+  }
+}
