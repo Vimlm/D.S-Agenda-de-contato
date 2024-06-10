e2c78c71636e8cacf01dc484fac313fb63b65d39
MileeMouse <ma.milecris@gmail.com>
2024-06-10 18:18:33 -0300

    💜Mile: Classe Anotações

---
+++ src/br/edu/up/model/Anotacoes.java
@@ -0,0 +1,19 @@
+package br.edu.up.model;
+import java.util.Date;
+
+public class Anotacoes {
+  private String texto;
+  private Date data;
+
+  public Anotacoes(String texto, Date data) {
+    this.texto = texto;
+    this.data = data;
+  }
+
+  public String getTexto() {
+    return texto;
+  }
+
+  public void setTexto(String texto) {
+    this.texto = texto;
+  }
