fb38f8f183caf698f636cf3c41ec44f3ee48e39d
SOMACAL <crdacostacaio@gmail.com>
2024-06-11 16:02:05 -0300

    Contato/endereço

--- src/br/edu/up/model/Contato.java
+++ src/br/edu/up/model/Contato.java
@@ -1,47 +1,59 @@
 package br.edu.up.model;
 
+import br.edu.up.model.Endereco;
+
 public class Contato {
-  private String nome;
-  private String email;
-  private Telefone telefone;
-  private ArrayList<Anotacoes> anotacoes;
+    private String nome;
+    private String email;
+    private Telefone telefone;
+    private ArrayList<Anotacoes> anotacoes;
+    private Endereco endereco;
 
-  public Contato(String nome, String email, Telefone telefone, ArrayList<Anotacoes> anotacoes) {
-    this.nome = nome;
-    this.email = email;
-    this.telefone = telefone;
-    this.anotacoes = anotacoes;
-  }
-  
-  public String getNome() {6
-    return nome;
-  }
+    public Contato(String nome, String email, Telefone telefone, ArrayList<Anotacoes> anotacoes, Endereco endereco) {
+        this.nome = nome;
+        this.email = email;
+        this.telefone = telefone;
+        this.anotacoes = anotacoes;
+        this.endereco = endereco;
+    }
 
-  public void setNome(String nome) {
-    this.nome = nome;
-  }
+    public String getNome() {
+        return nome;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public Telefone getTelefone() {
-    return telefone;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public void setTelefone(Telefone telefone) {
-    this.telefone = telefone;
-  }
+    public Telefone getTelefone() {
+        return telefone;
+    }
 
-  public ArrayList<Anotacoes> getAnotacoes() {
-    return anotacoes;
-  }
+    public void setTelefone(Telefone telefone) {
+        this.telefone = telefone;
+    }
 
-  public void setAnotacoes(ArrayList<Anotacoes> anotacoes) {
-    this.anotacoes = anotacoes;
-  }
+    public ArrayList<Anotacoes> getAnotacoes() {
+        return anotacoes;
+    }
+
+    public void setAnotacoes(ArrayList<Anotacoes> anotacoes) {
+        this.anotacoes = anotacoes;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
 }
